e6f5aee3bb460606f8a35cafd4432d887d413402
hochej <37776098+hochej@users.noreply.github.com>
2020-10-29 17:14:59 +0100

    initial commit. files were moved out of tincr

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+mod curfit;
+mod fpcurf;
+mod fpbspl;
+mod fpgivs;
+mod fprota;
+mod fpback;
+mod fprati;
+mod fpdisc;
+mod fpknot;
+mod fpchec;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
